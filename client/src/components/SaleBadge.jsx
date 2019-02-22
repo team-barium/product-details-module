8fb3ec4ef702da691f3a30d1cbe2b1b2529da31e
jmkchoi <jstnch24@gmail.com>
2019-02-21 18:23:32 -0800

    edit container max height, only display sale badge if on sale

--- client/src/components/SaleBadge.jsx
+++ client/src/components/SaleBadge.jsx
@@ -2,13 +2,17 @@ import React from 'react';
 import style from '../styles/saleBadge.css';
 
 const SaleBadge = ({ sale }) => {
-  return (
-    <div className={style.container}>
-      <div className={style.badge}>
-        -{sale} %
+  if (sale > 0) {
+    return (
+      <div className={style.container}>
+        <div className={style.badge}>
+          -{sale} %
+        </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return null;
+  }
 }
 
 export default SaleBadge;
