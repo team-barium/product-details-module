f1795972fe652f3ebcffcf9089a6295191329bf0
jmkchoi <jstnch24@gmail.com>
2019-02-23 14:42:19 -0800

    fix typos

--- client/src/components/QuantityDropdown.jsx
+++ client/src/components/QuantityDropdown.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import style from '../styles/QuantityDropdown.css';
+import style from '../styles/quantityDropdown.css';
 
 class QuantityDropdown extends React.Component {
   constructor(props) {
