b2485d3f13d1e35bfc9f55692880d7aae786b128
jmkchoi <jstnch24@gmail.com>
2019-02-24 00:46:29 -0800

    show currently selected index on indicator

--- client/src/components/CarouselIndicator.jsx
+++ client/src/components/CarouselIndicator.jsx
@@ -1,18 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import style from '../styles/carouselIndicator.css';
 
-const CarouselIndicator = ({ images, setIndex }) => (
-  <div className={style.container}>
-    <ul className={style.indicatorList}>
-      {images.map((image, key) => {
-        return (
-          <li key={key} index={key} className={style.indicator} onClick={setIndex}>
-            <a className={style.line} index={key} ></a>
-          </li>
-        );
-      })}
-    </ul>
-  </div>
-);
+const CarouselIndicator = ({ index, images, setIndex }) => {
+  const [current, setCurrent] = useState(index);
+  return (
+    <div className={style.container}>
+      <ul className={style.indicatorList}>
+        {images.map((image, key) => {
+          let indicatorStyle = key === current ? style.selectedIndicator : style.indicator;
+          return (
+            <li key={key} index={key} className={indicatorStyle} onClick={(e) => { setIndex(e); setCurrent(key); }}>
+              <a className={style.line} index={key} ></a>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
 
 export default CarouselIndicator;
