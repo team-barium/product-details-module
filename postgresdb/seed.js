ba6873d581d1bc3e369b05e0134a3f16fb3a212c
Mina Sorsok <mina.sorsok@gmail.com>
2019-03-02 10:33:31 -0800

    Organise data generation for each field

---
+++ postgresdb/seed.js
@@ -0,0 +1,27 @@
+const { ProductPromise, sequelize } = require('./index.js');
+const generateData = require('./generateData');
+const path = require('path');
+const fs = require('fs');
+const Promise = require('bluebird');
+
+ProductPromise.then(Product => {
+  Product.findAll({ limit: 10 }).then(result => {
+    if (result.length === 0) {
+      console.log('db empty beginning data generation');
+      generateData().then(() => {
+        console.log('data generated, beginning seeding');
+        sequelize
+          .query(
+            `COPY products FROM '${path.resolve(
+              __dirname,
+              './data.csv'
+            )}' WITH (FORMAT csv);`
+          )
+          .spread(() => {
+            fs.unlink(path.resolve(__dirname, './data.csv'), () => {});
+            console.log('seeding complete, deleting csv');
+          });
+      });
+    }
+  });
+});
