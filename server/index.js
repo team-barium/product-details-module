1af9c1ea8720da41dc6eaecdf5777f675d0d31f7
Mina Sorsok <mina.sorsok@gmail.com>
2019-03-05 19:14:50 -0800

    Refactor to use port from env file

--- server/index.js
+++ server/index.js
@@ -1,3 +1,9 @@
 const app = require('./app');
+const dotenv = require('dotenv');
+const dotenvExpand = require('dotenv-expand');
+const myEnv = dotenv.config();
+dotenvExpand(myEnv);
 
-app.listen(PORT, () => console.log('Listening on PORT', PORT));
+app.listen(process.env.PORT, () =>
+  console.log('Listening on PORT', process.env.PORT)
+);
